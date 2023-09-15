f1652e3c8b31cd5fa4047517199157ba8952869b
karanu-171 <karanujohn171@gmail.com>
2023-09-15 09:51:09 +0300

    final commit

--- src/main/java/com/karanu/crudApp/controller/StudentController.java
+++ src/main/java/com/karanu/crudApp/controller/StudentController.java
@@ -3,8 +3,8 @@ package com.karanu.crudApp.controller;
 import com.karanu.crudApp.entity.Student;
 import com.karanu.crudApp.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +15,35 @@ public class StudentController {
     // get all students
     @GetMapping("/students")
     public List<Student> getAllStudents(){
-        return null;
+        List<Student> students = repo.findAll();
+        return students;
+    }
+    // get one student
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id) {
+        Student student = repo.findById(id).get();
+        return student;
+    }
+
+    // create student
+    @PostMapping("student/add")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void createStudent(@RequestBody Student student) {
+        repo.save(student);
+    }
+    //update student
+    @PutMapping("student/update/{id}")
+    public Student updateStudent(@PathVariable int id) {
+       Student student = repo.findById(id).get();
+       student.setName("mjoni");
+       student.setPercentage(88);
+       repo.save(student);
+       return student;
+    }
+    //delete student
+    @DeleteMapping("student/delete/{id}")
+    public void removeStudent(@PathVariable int id) {
+        Student student = repo.findById(id).get();
+        repo.delete(student);
     }
 }
